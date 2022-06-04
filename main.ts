ab05381f0e88f40eda9b706d60bcf6a471b76556
dongnv0808 <yamaraja0808@gmail.com>
2022-06-04 23:28:52 +0700

    done

---
+++ main.ts
@@ -0,0 +1,17 @@
+let numbers: number[] = [0, 2, 3, 5, 6, 8, 9, 10]
+let output: number[] = [];
+let i = 0;
+// for(let i = 0; i <= 10; i++){
+//     if(numbers.indexOf(i) == -1){
+//         output.push(i);
+//         count++;
+//     }
+// }
+while(i < numbers.length){
+    if(numbers.indexOf(i) == -1){
+        output.push(i);
+    }
+    i++;
+}
+console.log(output);
+    
